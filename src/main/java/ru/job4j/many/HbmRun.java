7c335397c0f9f9cc0fa450832bb5df798f0cb20a
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-10-31 16:07:59 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/many/HbmRun.java
@@ -0,0 +1,34 @@
+package ru.job4j.many;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.many.model.car.Mark;
+import ru.job4j.many.model.car.Model;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Mark bmw = Mark.of("BMW");
+            for (int i = 1; i <= 5; i++) {
+                Model model = Model.of(i + " series");
+                session.save(model);
+                bmw.addModel(model);
+            }
+            session.save(bmw);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/many/model/car/Mark.java
@@ -0,0 +1,80 @@
+package ru.job4j.many.model.car;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "marks")
+public class Mark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(unique = true)
+    private int id;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Model> models = new ArrayList<>();
+
+    public Mark() {
+    }
+
+    public static Mark of(String name) {
+        Mark mark = new Mark();
+        mark.setName(name);
+        return mark;
+    }
+
+    public List<Model> getAllModels() {
+        return models;
+    }
+
+    public void addModel(Model model) {
+        this.models.add(model);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Mark mark = (Mark) o;
+        return id == mark.id && Objects.equals(name, mark.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Mark{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
