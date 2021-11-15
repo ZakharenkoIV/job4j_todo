94dbf3cd87e40584904baa2ba61d0e733c1018ec
ZakharenkoIV <zaharenkoiv93@gmail.com>
2021-11-15 15:40:24 +0300

    Модели и связи. Машины и владельцы [#4744]

---
+++ src/main/java/ru/job4j/cars/Driver.java
@@ -0,0 +1,11 @@
+package ru.job4j.cars;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "driver")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
---
+++ src/main/java/ru/job4j/cars/Engine.java
@@ -0,0 +1,11 @@
+package ru.job4j.cars;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "engine")
+public class Engine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
